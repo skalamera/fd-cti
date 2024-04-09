e02496245528c4822ddccc4e4eeef175a1ef96a5
Raviraj Subramanian <raviraj.subramanian@freshworks.com>
2024-04-09 22:30:34 +0530

    Migrated the app to the platform v3.0 (#5)
    * Migrated to platform v2.3
    
    * Migrated to platform v3.0

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -18,7 +18,7 @@ function showNotify(type, message) {
  */
 function goToTicket(ticketId) {
   client.interface.trigger("click", { id: "ticket", value: ticketId })
-    .then(function (data) {
+    .then(function () {
       console.info('successfully navigated to ticket');
     }).catch(function (error) {
       console.error('Error: Failed to navigate to ticket');
@@ -33,7 +33,7 @@ function goToTicket(ticketId) {
  */
 function goToContact(contactId) {
   client.interface.trigger("click", { id: "contact", value: contactId })
-    .then(function (data) {
+    .then(function () {
       console.info('successfully navigated to contact');
     }).catch(function (error) {
       console.error('Error: Failed to navigate to contact');
@@ -48,7 +48,7 @@ function goToContact(contactId) {
  */
 function showMissedCall(missedCalls) {
   client.interface.trigger("show", { id: "missedCall", value: missedCalls })
-    .then(function (data) {
+    .then(function () {
       console.info('successfully shown missed calls');
     }).catch(function (error) {
       console.error('Error: failed to show missed calls');
@@ -61,7 +61,7 @@ function showMissedCall(missedCalls) {
  */
 function hideMissedCall() {
   client.interface.trigger("hide", { id: "missedCall" })
-    .then(function (data) {
+    .then(function () {
       console.info('successfully hidden missed calls');
     }).catch(function (error) {
       console.error('Error: failed to hide missed calls');
@@ -75,21 +75,18 @@ function hideMissedCall() {
 async function createTicket() {
   const callNotes = "Alice called Bob"
   const ticketDetails = {
-    email: 'sample@email.com',
-    subject: 'Call with the customer',
-    priority: 1,
-    description: `Ticket from call. Call Notes:${callNotes}`,
-    status: 2
-  }
+      email: 'sample@email.com',
+      subject: 'Call with the customer',
+      priority: 1,
+      description: `Ticket from call. Call Notes:${callNotes}`,
+      status: 2
+    };
   try {
-    const ticketData = await client.request.post('https://<%= iparam.freshdesk_subdomain %>.freshdesk.com/api/v2/tickets',
-      {
-        headers: {
-          Authorization: '<%= encode(iparam.freshdesk_api_key) %>'
-        },
-        json: ticketDetails,
-        method: "POST"
-      });
+    let ticketData = await client.request.invokeTemplate("createTicket", {
+      body: JSON.stringify(ticketDetails)
+    });
+    let responseJSON = JSON.parse(ticketData.response);
+    console.log(responseJSON);
     console.info('Successfully created ticket in Freshdesk');
     showNotify('success', 'Successfully created a ticket.');
   } catch (error) {
@@ -102,47 +99,40 @@ async function createTicket() {
 /**
  * It retrieves the list of contacts from Freshdesk
  */
-function getContacts() {
-  const url = 'https://<%= iparam.freshdesk_subdomain %>.freshdesk.com/api/v2/contacts';
-  const options = {
-    headers: {
-      Authorization: 'Basic <%= encode(iparam.freshdesk_api_key) %>'
-    }
-  }
-  client.request.get(url, options).then(contacts => {
-    console.info('Success: Got contacts list');
+async function getContacts() {
+  try {
+    let contacts = await client.request.invokeTemplate("getContacts");
     console.table(JSON.parse(contacts.response));
     return JSON.parse(contacts.response);
-  }, error => {
+  } catch (error) {
+    console.error('Error: Failed to get contacts from Freshdesk');
     console.error(error);
-    return reject(error);
-  });
+    return error;
+  }
 }
 
 /**
  * It creates a contact in Freshdesk with the phone number
  */
-function createContact() {
+async function createContact() {
   const properties = {
     name: 'contact name',
     email: 'sample2@email.com',
     phone: '+0123456789',
 
   }
-  const url = 'https://<%= iparam.freshdesk_subdomain %>.freshdesk.com/api/v2/contacts';
-  const options = {
-    headers: {
-      Authorization: 'Basic <%= encode(iparam.freshdesk_api_key) %>'
-    },
-    json: properties
-  }
-  client.request.post(url, options).then(contact => {
-    console.info('Success: Created contact');
-    console.info(contact.response);
-  }, error => {
-    console.error('Error: Failed to create contact');
+
+  try {
+    let response = await client.request.invokeTemplate("createTicket", {
+      body: JSON.stringify(properties)
+    });
+    console.table(JSON.parse(response.response));
+    return JSON.parse(response.response);
+  } catch (error) {
+    console.error('Error: Failed to create a contact in Freshdesk');
     console.error(error);
-  });
+    return error;
+  }
 }
 
 /**
@@ -177,7 +167,7 @@ function openApp() {
  * To close the CTI app
  */
 function closeApp() {
-  client.interface.trigger("hide", { id: "softphone" }).then(function (data) {
+  client.interface.trigger("hide", { id: "softphone" }).then(function () {
     console.info('successfully closed the CTI app');
     showNotify('success', 'Successfully closed the CTI app.');
   }).catch(function (error) {
